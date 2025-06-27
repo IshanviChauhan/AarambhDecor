a50ca9d31c5ee4065a6741fc09af29595967ad90
Ishanvi Chauhan <ishanvi.chauhan@gmail.com>
2025-06-27 22:23:15 +0000

    nodemon] starting `node index.js` [0] [0]   VITE v5.4.19  ready in 605

--- Backend/index.js
+++ Backend/index.js
@@ -12,14 +12,14 @@ require("dotenv").config();
 const uploadImage = require("./utils/uploadimage");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.use(helmet());
 
 // CORS Configuration
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL || "http://localhost:5174" ],
+    origin: [process.env.FRONTEND_URL || "http://localhost:5173" ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })
