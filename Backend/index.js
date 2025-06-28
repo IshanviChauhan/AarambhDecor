6e6d42d507e473d0e10d8c65e336ab2deaae190e
Ishanvi Chauhan <ishanvi.chauhan@gmail.com>
2025-06-28 07:43:31 +0000

    0]   VITE v5.4.19  ready in 3271 ms [0] [0]   ➜  Local:   http://localh

--- Backend/index.js
+++ Backend/index.js
@@ -12,14 +12,14 @@ require("dotenv").config();
 const uploadImage = require("./utils/uploadimage");
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4001;
 
 app.use(helmet());
 
 // CORS Configuration
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL || "http://localhost:5173" ],
+    origin: [process.env.FRONTEND_URL || "http://localhost:5174" ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })
